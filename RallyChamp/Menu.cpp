eb8704a6abdd589f115fd2c418c379271172058f
surpr1se <86316775+Surpr1se0@users.noreply.github.com>
2022-03-07 02:09:02 +0000

    Add menu

---
+++ RallyChamp/Menu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by surprise on 07/03/22.
+//
+
+#include "Menu.h"
---
+++ RallyChamp/Menu.h
@@ -0,0 +1,23 @@
+//
+// Created by surprise on 07/03/22.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <cstdlib>
+
+using namespace std;
+
+#ifndef RALLYCHAMP_MENU_H
+#define RALLYCHAMP_MENU_H
+
+class Menu
+{
+	//private 
+		//Rally Rally_A;
+public:
+		
+};
+
+#endif //RALLYCHAMP_MENU_H
