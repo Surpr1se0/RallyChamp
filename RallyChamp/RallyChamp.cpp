c56e9563df15873943d30b5b41b79abe57734edf
Francisco Gouveia <86316775+Surpr1se0@users.noreply.github.com>
2022-03-08 16:01:14 +0000

    Update new notes in Readme.md

--- RallyChamp/RallyChamp.cpp
+++ RallyChamp/RallyChamp.cpp
@@ -7,6 +7,11 @@ Para detetarmos se ha um carro igual existente no concurso implementei a Car::Eq
 Um piloto pode conduzir com mais que um carro?
 Na classe Team, temos de colocar Pilotos e Carros, sendo que Carros já tem Pilotos Associados?
 
+//QT:
+No construtor temos de criar um Menu
+A classe Menu deve ter a opção de criar um rally, lista-los e administra-los
+
+
  -- Ficheiro Rascunho e UML--
  https://drive.google.com/file/d/19clDbo6Nc3xcgCdEw7XBXxDFoQeEXgtk/view?usp=sharing
 
